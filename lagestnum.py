6d8ce71d699e26ab9aab8b25ce4c7e8c79c3ec81
Yakhubji Rabbani <yakhubji.r@ivygdcad.ivy-support.com>
2020-01-24 12:42:34 +0530

    ss

--- lagestnum.py
+++ lagestnum.py
@@ -15,5 +15,5 @@ elif (num1 >= num2) and (num1 >= num3):
 elif (num2 >= num1) and (num2 >= num3):
    largest = num2
 else:
-   largest = num3
+   //largest = num3
 print("The largest number is", largest)
